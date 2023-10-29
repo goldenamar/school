b556484cd268df43fdca4a52a8d1cdd9ca4d1b17
Amarchingun <amarchingun@Byambadorjs-MacBook-Pro.local>
2023-10-29 21:17:15 +0800

    added changes to front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,16 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <div className="header-items">9C Breakfast Sale</div>
-        <div className="header-items">Contact & Info</div>
-        <div className="header-items">Products</div>
-        <div className="header-items">Pre-Order</div>
-        <div className="header-items">Sign-in</div>
-      </header>
-      <div className="mid">
-        <div className="mid-item"></div>
-      </div>
+      
     </div>
   );
 }
