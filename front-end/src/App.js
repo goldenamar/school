5c956f09260c0a78ce6448238fe61462ce2e3ce0
Amarchingun <amarchingun@Byambadorjs-MacBook-Pro.local>
2023-10-29 21:49:02 +0800

    changes to front-end aswell

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="header"></div>
+      <div className="main"></div>
+      <div className="end"></div>
     </div>
   );
 }
