c1eef034d6d5d67ee09b1850b9da7e2d9123b584
Amarchingun <amarchingun@Byambadorjs-MacBook-Pro.local>
2023-11-01 15:05:41 +0800

    first step to completion

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="header"></div>
-      <div className="main"></div>
-      <div className="end"></div>
+      <div className="header">
+        <div className="logo">WWWORX</div>
+        <div className="menu">
+          <div className="menu-item">Home</div>
+          <div className="menu-item">Our Work</div>
+          <div className="menu-item">Contact</div>
+          <div className="menu-item">Login</div>
+        </div>
+      </div>
+      <hr></hr>
     </div>
   );
 }
